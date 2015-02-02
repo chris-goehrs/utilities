7f8161dd8f3381ca2c7b8528495d80a7c357e3e5
Chris Goehrs <chris@wisetoweb.com>
2015-02-02 16:31:42 -0600

    Changed the location of the resources folder

--- Resources/country_list.sql
+++ Resources/country_list.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.0.9
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Jan 24, 2015 at 07:42 PM
--- Server version: 5.6.14
--- PHP Version: 5.5.6
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -15,10 +6,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `cworld`
---
 
 -- --------------------------------------------------------
 
